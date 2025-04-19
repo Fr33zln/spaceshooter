ceb0dc589539c9d270bb746a696f1828f9d7f924
Fr33zln <fre3ezln@gmail.com>
2025-04-19 11:42:07 +0300

    Update data.py

--- data.py
+++ data.py
@@ -5,51 +5,43 @@ from random import randint, choice
 
 pygame.init()
 
-size_window = (995,500)
+size_window = (50,600)
 size_background = (600,3000)
-size_hero = (60,45)
-size_bot = (60,45)
+size_hero = (70,50)
+size_bot = (42,48)
 size_buff = (50,50)
 # 
 
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+RED = (255,0,0)
+BLUE = (0,0,255)
+GREEN = (0,255,0)
+FPS = 60
+COUNT_BUFF = 5
+BUFFS = ["HP","speed_bullet","speed_shoot","immortal","freezing"]
 
-FPS = 144
-#15 15
-well_list = list()
-wall_list = list()                
+heart_list = list()     
+bot_list = list()      
 bullet_list_hero = list()
 bullet_list_bot = list()
-bot_list = list()b
 
 
-
-
-BUFFS = ["HP", "speed_bullet", "speed_shoot","immortal","freezing"]
 abs_path = os.path.abspath(__file__ +"/..")
-hero_image_list = [
-    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "noob_stand.png")), size_hero),
-    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "noob_move1.png")), size_hero),
-    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "noob_move2.png")), size_hero)
-]
-bot1_image_list = [
-    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "zombie_stand.png")), size_bot),
-    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "zombie_move1.png")), size_bot),
-    pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "zombie_move2.png")), size_bot)
-]
 
+hero_image_list = pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "hero.png")), size_hero)
+
+bot_image_list = pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "bot.png")), size_bot)
 
 
 heart_image = pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "heart.png")), [30,30])
 skull_image = pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "skull.png")), [30,30])
                        
 
-
 background_image =  pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "background.png")), size_background)
 
-buffs_images = [
+buff_images = [
 pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "background.png")), size_background),
 pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "b_bullet_limit.png")), size_buff),
 pygame.transform.scale(pygame.image.load(os.path.join(abs_path, "Images", "b_bullet_speed.png")), size_buff),
